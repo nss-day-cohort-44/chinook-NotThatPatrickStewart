15933a6de323f807f4eaa211cdd5fd65b975bfa2
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-02-26 11:09:36 -0600

    fixed query for line_item_track_artist.sql

--- sql exercises/line_item_track_artist.sql
+++ sql exercises/line_item_track_artist.sql
@@ -1,7 +1,6 @@
-SELECT COUNT(*) AS 'Line_Item', Track.Name AS 'Track_Name', Composer AS 'Artist_Name'
+SELECT InvoiceLineId AS 'Line_Id', Track.Name AS 'Track_Name', Composer AS 'Artist_Name'
     FROM InvoiceLine
     JOIN Track
-        ON InvoiceLine.TrackId = Track.TrackId
-    GROUP BY InvoiceId
-
-/*Without right join being supported, is there a way to get rid of NULL artist displays?*/
+        ON InvoiceLine.TrackId = Track.TrackId;
+    
+/*WHERE Composer IS NOT NULL;*/
